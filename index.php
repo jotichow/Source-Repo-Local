ef0686b1c0052f8a15b28865f15b6168bd283beb
Joti Chow <jotichow@yahoo.com>
2019-07-21 23:56:24 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis sapiente expedita, sit fugiat error natus molestiae dolor, nobis odit provident corporis assumenda blanditiis eligendi eos ipsa quo alias commodi. Voluptatum?</p>
+    <ul>
+      <li>Satu</li>
+      <li>Dua</li>
+    </ul>
 </body>
 </html>
